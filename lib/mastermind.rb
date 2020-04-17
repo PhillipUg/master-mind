521e0ed561746e6f747f18f6b738cfd9d7ea31ea
phillip musiime <phillipmusiime@gmail.com>
2020-04-17 12:51:09 +0300

    added num_of_guesses & letters properties to improve overall gameplay and output

--- lib/mastermind.rb
+++ lib/mastermind.rb
@@ -1,8 +1,16 @@
 require_relative "code"
 
+
+
 class Mastermind
+  attr_reader :num_of_guesses, :secret_code
+
   def initialize(length)
+    @letters = %w(A B C D)
+    @length = length
     @secret_code = Code.random(length)
+    @sample = Array.new(@length, @letters.sample)
+    @num_of_guesses = 0
   end
 
   def print_matches(code)
@@ -11,10 +19,12 @@ class Mastermind
   end
 
   def ask_user_for_guess
-    puts "Enter a code"
+    print "Enter a pattern of #{@length} letters (e.g '#{@sample.join}'): "
     answer = gets.chomp
     guess = Code.from_string(answer)
     self.print_matches(guess)
-    @secret_code == guess 
+    @num_of_guesses += 1
+    @secret_code == guess
   end
+
 end
